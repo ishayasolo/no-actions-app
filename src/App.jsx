9452edce99a88e6fe26e3866764a611345fab322
ishayasolo <ishaya808@gmail.com>
2023-02-01 23:44:24 +0100

    cleanup code files

--- src/App.jsx
+++ src/App.jsx
@@ -26,20 +26,26 @@ function App() {
   };
 
   return (
-    <div style={{ padding: '16px' }}>
-      <h2>You can highlight the text below but you can't copy</h2>
+    <div style={{ padding: '16px', maxWidth: '600px' }}>
+      <h2 style={{ lineHeight: '1.5' }}>
+        You can highlight the text below but you can't copy any of it's content
+      </h2>
       <Paragraph />
 
       <br />
 
       <NoHighlight>
-        <h2>You can't highlight or copy the text below</h2>
+        <h2 style={{ lineHeight: '1.5' }}>
+          You can't highlight or copy the text below
+        </h2>
         <Paragraph />
       </NoHighlight>
 
       <br />
 
-      <h2>You can't perform any click actions on the below images</h2>
+      <h2 style={{ lineHeight: '1.5' }}>
+        You can't download or perform any click actions on the images below
+      </h2>
       <div
         style={{
           display: 'flex',
@@ -49,7 +55,7 @@ function App() {
           maxWidth: '600px',
         }}
       >
-        {images.map(image => (
+        {images.map((image, index) => (
           <div
             key={image.id}
             style={{
@@ -60,12 +66,17 @@ function App() {
           >
             <Image
               src={image.url}
-              alt="a cat image"
+              alt={`cat image number ${index + 1}`}
             />
           </div>
         ))}
       </div>
-      <h2>You can't download or right-click on the video below</h2>
+
+      <br />
+
+      <h2 style={{ lineHeight: '1.5' }}>
+        You can't download or right-click on the video below
+      </h2>
       <div onContextMenu={handleContextMenu}>
         <video
           src="https://rr1---sn-huoob-avnl.googlevideo.com/videoplayback?expire=1675291341&ei=bZbaY_2kNqKpobIPyt6XoAU&ip=168.232.160.14&id=o-AGSsUA8lquz8haNcG8jQIE8sPiZdEDLnEw2YNqkF4f2U&itag=18&source=youtube&requiressl=yes&spc=H3gIhnC-EEnkWShhXTwyi6SNvcxA1o4&vprv=1&mime=video%2Fmp4&ns=sMGnomxENuQCIKKtv6xeUmcL&gir=yes&clen=43311040&ratebypass=yes&dur=615.125&lmt=1669347190111837&fexp=24007246,24462247&beids=24462247&c=WEB&txp=5530434&n=WtdDWELuNCJGYg&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cspc%2Cvprv%2Cmime%2Cns%2Cgir%2Cclen%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRgIhAJAgKsksq1VA0xKDt0Uk03ubP7LLYyGXG7aIQlY946ttAiEAqJ9s_hlQWK8vxvhbuHDw00_FmFLMAF4jaUrDHh0gYWc%3D&redirect_counter=1&rm=sn-bg0yd76&req_id=85adf74cc034a3ee&cms_redirect=yes&ipbypass=yes&mh=BE&mip=102.89.44.252&mm=31&mn=sn-huoob-avnl&ms=au&mt=1675278339&mv=m&mvi=1&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRQIgSLNIg3PKWamiaLnFmvHvJAXVOqpccVsC4n5yu9BLElMCIQDeR70nmEPmTp7-EWxIRpOg6YrVFWiMTBistJ48C5t0Bw%3D%3D"
